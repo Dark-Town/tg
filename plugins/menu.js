8922c8b8f81b5fca334f1a2d144fe4529a6e1249
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-14 12:15:27 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,15 +6,34 @@ Command({
     fromAdmin: true // You can change this to false if you want all users to access the menu
 }, async (message, match) => {
     const menuText = `
-*Available Commands:*
-1. /ping - Check the bot's responsiveness.
-2. /help - Get help with using the bot.
-3. /info - Get information about the bot.
-4. /settings - Change your user settings.
-5. /about - Learn more about this bot.
-
-*Note:* Use the command prefix before each command.
-`;
-
-    await message.reply(menuText, { parse_mode: "Markdown" });
+╭────────❒⁠⁠⁠⁠ *PAID TECH SYSTEM* ➣
+│➣│▸ Ping
+│➣│▸ Runtime
+│➣│▸ Jid
+│➣│▸ Enc
+│➣│▸ Pkg
+│➣│▸ Install 
+│➣│▸ Beautify
+╰────────❍─────❍❍➣
+╭────────❒⁠⁠⁠⁠ *PAID TECH* ➣
+│➣│▸ MENU
+│➣│▸ HELP
+│➣│▸ SETTINGS
+ ╰────────❍─────❍❍➣
+╭────────❒⁠⁠⁠⁠ *PAID-𝙻𝙾𝙶 𝙼𝙰𝙺𝙴𝚁* ➣
+│➣│▸ LOG
+╰────────❍─────❍❍➣ 
+╭────────❒⁠⁠⁠⁠ *PAID TECH SOCIAL* ➣
+│➣│▸ TIKTOK
+│➣│▸ SONG
+│➣│▸ VIDEO 
+╰────────❍─────❍❍➣
+╭────────❒⁠⁠⁠⁠ *PAID TECH AI* ➣
+│➣│▸ GPT
+╰────────❍─────❍❍➣
+ ╭────────❒⁠⁠⁠⁠ *PAID TECH +18* ➣
+│➣│▸ HNEKO
+│➣│▸ HWAIFU
+╰────────❍─────❍❍➣ 
+  > © 𝑀𝐴𝐷𝐸 𝐵𝑌 𝑇𝐶𝑅𝑂𝑁𝐸𝐵 𝐻𝐴𝐶𝐾𝑆🤍 `;    await message.reply(menuText, { parse_mode: "Markdown" });
 });
